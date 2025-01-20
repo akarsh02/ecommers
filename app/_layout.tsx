240c5629ef798d326ad513e70b278b5b7fb7d9cc
akarsh <akarsh226@gmail.com>
2025-01-20 15:53:29 +0530

    install gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Stack, Tabs } from 'expo-router'
+import "@/global.css";
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
+
 
 const RootLayout = () => {
  return (
-  <>
+  <GluestackUIProvider>
    <Stack />
-  </>
+  </GluestackUIProvider>
  )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,17 @@ import { FlatList, Text } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import products from '../assets/products.json'
 import ProductListItem from '../components/ProductListItem'
+import { Button, ButtonText } from '@/components/ui/button'
 
 
 
 const HomeScreen = () => {
  return (
   <>
-   <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item} />} />
+   {/* <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item} />} /> */}
+   <Button variant='solid'>
+    <ButtonText>Click me</ButtonText>
+   </Button>
   </>
  )
 }
