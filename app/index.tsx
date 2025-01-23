f7ac6baa0a63a643e4a609378d1d388c475cb891
akarsh <akarsh226@gmail.com>
2025-01-23 18:08:36 +0530

    fix web and make it responsive

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,18 @@ import { StatusBar } from 'expo-status-bar'
 import products from '../assets/products.json'
 import ProductListItem from '../components/ProductListItem'
 import { Button, ButtonText } from '@/components/ui/button'
+import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value'
 
 
 
 const HomeScreen = () => {
  const { width } = useWindowDimensions()
- const numColumns = width > 700 ? 3 : 2;
+ // const numColumns = width > 700 ? 3 : 2;
+ const numColumns = useBreakpointValue({
+  default: 3,
+  sm: 3,
+  xl: 4
+ })
  return (
   <>
    <FlatList data={products} className=' max-w-[960px] mx-auto' numColumns={numColumns} key={numColumns} columnWrapperClassName='gap-2' contentContainerClassName='gap-2' renderItem={({ item }) => <ProductListItem product={item} />} />
